95a37167ba26da250e989b8df2cc9c42bd2cdf7b
txting <johnny6night@gmail.com>
2018-08-14 16:18:20 +0800

    add static and media files

--- posts/views.py
+++ posts/views.py
@@ -6,10 +6,15 @@ from django.utils import timezone
 from oauth2_provider.views.generic import ProtectedResourceView
 from django.http import HttpResponse
 
-#@login_required()
+
 class ApiEndpoint(ProtectedResourceView):
     def get(self, request, *args, **kwargs):
         return HttpResponse('Hello, OAuth2!')
+
+@login_required()
+def secret_page(request, *args, **kwargs):
+    return HttpResponse('Secret contents!', status=200)
+
 
 
 def homepage(request):
